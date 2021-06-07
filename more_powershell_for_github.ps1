8ddb6b443309407b4bd0f44309362c95eb9b5afb
s115345 <s115345@ap.be>
2021-06-08 01:49:54 +0200

    poging-header

---
+++ more_powershell_for_github.ps1
@@ -0,0 +1,10 @@
+﻿function Get-AuthHeader{
+    
+    param($Credentials)
+    $Base64Token = [System.Convert]::ToBase64String([char[]]$Token);
+    $headers = 
+    @{
+        Authorization = 'Basic {0}' -f $Base64Token;
+        
+    }
+    }
