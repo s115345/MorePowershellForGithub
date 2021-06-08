cfbabe03a3efbed3abe0dca9a1f3c5e32d7dc765
s115345 <s115345@ap.be>
2021-06-08 22:11:11 +0200

    add collaborators

--- more_powershell_for_github.ps1
+++ more_powershell_for_github.ps1
@@ -24,7 +24,37 @@
     
 
     Invoke-RestMethod -Headers $headers -Uri $api
+
     }
+
+function Add-GitHubCollaborator {
+
+
+
+param ($header,$Credential, $Repository, $Collaborator)
+
+
+Get-AuthHeader -Credential $Credential
+#https://api.github.com/repos/octocat/hello-world/collaborators
+#https://api.github.com/repos/octocat/hello-world/collaborators/USERNAME \
+
+$global:api = "https://api.github.com/repos/$Credential/$Repository/collaborators/$Collaborator/s115345@ap.be"
+#body aanmaken voor variablen
+$Body = 
+@{  repo =     $Repository;
+
+    username = $Collaborator;
+
+    owner =    $Credential;
+
+} | ConvertTo-Json
+
+#headers toevoegen  en ook de uri
+Invoke-RestMethod -Method Put -Headers $header -uri $api   
+#PUT method is call when you have to modify a single resource
+$repo = New-GitHubRepository `
+-RepositoryName test-morePS4Github
+}
 
 #Read-Host -AsSecureString -Prompt ’token’ |
 #ConvertFrom-SecureString |
