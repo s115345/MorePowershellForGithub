ca1eb8890cb21b9a53625d918869c4555c4edc69
s115345 <s115345@ap.be>
2021-06-08 02:23:35 +0200

    return-moet-nog-testen

--- more_powershell_for_github.ps1
+++ more_powershell_for_github.ps1
@@ -1,10 +1,21 @@
 ﻿function Get-AuthHeader{
     
     param($Credentials)
-    $Base64Token = [System.Convert]::ToBase64String([char[]]$Token);
+    $Authorisatie = [System.Convert]::ToBase64String([char[]]$Creds.GetNetworkCredential().Password)
     $headers = 
     @{
-        Authorization = 'Basic {0}' -f $Base64Token;
+        Authorization = 'Basic $Authorisatie'; }
+        return $headers
         
     }
-    }
+
+#Read-Host -AsSecureString -Prompt ’token’ |
+#ConvertFrom-SecureString |
+#Tee-Object ./secret.txt |
+#ConvertTo-SecureString
+#$creds = New-Object pscredential 'user’, $ss_token
+#Set-GitHubAuthentication -SessionOnly `
+#-Credential $creds
+#$api = "https://api.github.com/user"
+#Invoke-RestMethod -Headers $headers -Uri $api
+    
