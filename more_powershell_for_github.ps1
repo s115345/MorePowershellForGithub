14a9c5a9cbfb721c6950c034be84ff66ebc57d93
s115345 <s115345@ap.be>
2021-06-08 19:28:35 +0200

    basic-encoding-veranderen

--- more_powershell_for_github.ps1
+++ more_powershell_for_github.ps1
@@ -1,19 +1,24 @@
 ﻿function Get-AuthHeader{
     
  param($Credentials)
+ 
+    $Credential = Read-Host -Prompt ’login’
 
- Read-Host -AsSecureString -Prompt ’token’ |
- ConvertFrom-SecureString |
- Tee-Object ./secret.txt |
- ConvertTo-SecureString
- $creds = New-Object pscredential 'user’, $ss_token
- Set-GitHubAuthentication -SessionOnly `
- -Credential $creds
- $Authorisatie = [System.Convert]::ToBase64String([char[]]$Creds.GetNetworkCredential().Password)
- $api = "https://api.github.com/user"
- $headers = 
+
+    $access_token = Read-Host -AsSecureString -Prompt ’token’ |
+    
+    ConvertFrom-SecureString |
+    Tee-Object ./secret.txt |
+    ConvertTo-SecureString
+    $creds = New-Object pscredential 'user’, $access_token
+    Set-GitHubAuthentication -SessionOnly `
+    -Credential $creds
+
+    $Encoding= [System.Convert]::ToBase64String([char[]]$Creds.GetNetworkCredential().Password)
+    $global:api = "https://api.github.com/user"
+    $global:headers = 
     @{
-        Authorization = 'Basic $Authorisatie'; };
+        Authorization = 'Basic  $Encoding'; };
         return $headers
         
     
